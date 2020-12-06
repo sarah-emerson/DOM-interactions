5ca05b77dcb5239a6a0d55bb8fa875051093d313
Sarah Emerson <sarah.emerson@devacademy.co.nz>
2020-12-06 18:55:28 +1300

    Make dots disappear/appear on double-click

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ function bindEventListeners (dots) {
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
     dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -24,12 +25,15 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 function makeBlue (evt) {
-  evt.preventDefault()
   evt.target.classList.toggle('blue')
   updateCounts()
 }
 
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
